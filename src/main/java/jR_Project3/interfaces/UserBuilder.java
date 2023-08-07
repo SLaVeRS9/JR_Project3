3ad2e0805eb9e5196d625e6290d44dec36561b7f
SLaVeRS9 <SLaVeRS9@outlook.com>
2023-08-07 14:33:52 +0300

    blah

--- src/main/java/jR_Project3/interfaces/UserBuilder.java
+++ src/main/java/jR_Project3/interfaces/UserBuilder.java
@@ -1,9 +1,10 @@
 package jR_Project3.interfaces;
 
 import jR_Project3.models.users.AbstractUser;
-import jR_Project3.models.users.User;
 
 public interface UserBuilder {
     public UserBuilder name(String name);
     public AbstractUser build();
+
+    public String getName();
 }
