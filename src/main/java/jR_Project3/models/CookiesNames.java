aec113be472338ee9b5297a9490e3a9ae6c2e5c3
SLaVeRS9 <SLaVeRS9@outlook.com>
2023-08-08 00:08:13 +0300

    Added CookiesNames

---
+++ src/main/java/jR_Project3/models/CookiesNames.java
@@ -0,0 +1,14 @@
+package jR_Project3.models;
+
+public enum CookiesNames {
+    USER_NAME_COOKIE("UserName"),
+    USER_COUNT_ENTRANCES_COOKIE("UserEntrances");
+    private String cookieName;
+    CookiesNames(String cookieName) {
+        this.cookieName = cookieName;
+    }
+
+    public String getName(){
+        return cookieName;
+    }
+}
