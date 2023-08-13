3dcb1abee4d5d891c9b0f584a1fc8b657e877fe0
SLaVeRS9 <SLaVeRS9@outlook.com>
2023-08-14 01:26:07 +0300

    Refactored CookiesNames

--- src/main/java/jR_Project3/models/CookiesNames.java
+++ src/main/java/jR_Project3/models/CookiesNames.java
@@ -3,7 +3,7 @@ package jR_Project3.models;
 public enum CookiesNames {
     USER_NAME_COOKIE("UserName"),
     USER_COUNT_ENTRANCES_COOKIE("UserEntrances");
-    private String cookieName;
+    private final String cookieName;
     CookiesNames(String cookieName) {
         this.cookieName = cookieName;
     }
